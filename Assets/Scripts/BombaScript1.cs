30d942231aa411179d7fd932d2f328cb16e6f2b5
KorbenKross <kross19981945@gmail.com>
2018-02-23 21:27:23 +0400

    message

---
+++ Assets/Scripts/BombaScript1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombaScript1 : MonoBehaviour {
+
+    // Как долго существует лазер
+    public float lifetime = 2.0f;
+
+    // Как много наносит урона лазер при соприкосновении с врагами
+    public int damage = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Уничтожение лазера по окончанию таймера
+        Destroy(gameObject, lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(Vector3.up * Time.deltaTime);
+    }
+}
